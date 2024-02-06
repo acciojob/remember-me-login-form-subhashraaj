0ddafb0823be36f2eb8fa0845463f6eb4211243d
Subhash Raaj K <150801940+subhashraaj@users.noreply.github.com>
2024-02-06 11:37:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+const loginForm = document.querySelector("#loginForm");
+const username = document.querySelector("#username").value;
+const password = document.querySelector("#password").value;
+const checkbox = document.querySelector("#checkbox");
+const submit = document.querySelector("#submit");
+let userExist = false;
+
+window.addEventListener("load", () => {
+    loginForm.style.display = "flex";
+});
+function checkBoxFunction() {
+    if (checkbox.checked == true) {
+        return true
+    }
+};
+submit.addEventListener("click", (e) => {
+    e.preventDefault()
+    //console.log(checkBoxFunction())
+    alert("Logged in as.")
+    if (checkBoxFunction()) {
+        const username = document.querySelector("#username").value;
+        const password = document.querySelector("#password").value;
+        localStorage.setItem("username", username);
+        localStorage.setItem("password", password);
+        userExist = true;
+    };
+});
+console.log(userExist)
+if(userExist === true){
+    document.querySelector("#existing").style.display="block";
+}
